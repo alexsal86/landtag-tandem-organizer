384d4b4e89693b4d77e6da9dd0758ca94e59499a
alexsal86 <50153375+alexsal86@users.noreply.github.com>
2025-09-24 10:38:28 +0200

    Update __init__.py

--- scripts/parser_core/__init__.py
+++ scripts/parser_core/__init__.py
@@ -1,10 +1,2 @@
-from .segment import segment_speeches
-from .toc_parser import parse_toc
-from .pipeline import parse_protocol, split_toc_and_body
-
-__all__ = [
-    "segment_speeches",
-    "parse_toc",
-    "parse_protocol",
-    "split_toc_and_body",
-]
+# Mark scripts as a package to allow imports like scripts.parser_core.pipeline
+__all__ = []
